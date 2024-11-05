44126c7b5c56382efa0d66bfdf01ac6116828eac
Ryanforce08 <rradtke1208@gmail.com>
2024-11-05 08:13:57 -0500

    lot of stuff

---
+++ Python/prog1/python1.py
@@ -0,0 +1,5 @@
+print("hello world")
+
+age = 15
+
+print("i am", age, "years old")
---
+++ Python/prog2/python2.py
@@ -0,0 +1,3 @@
+n = int(input("enter a number: "))
+name = input("Name: ")
+print("Number+5=",n+5,"number-2=",n-2,"number/2=",n/2,"number*2=",n*2,)
