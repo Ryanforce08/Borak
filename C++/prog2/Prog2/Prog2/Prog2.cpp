48c01c4883f1d0ae0a25e52222e8caba87b0fbb5
Ryanforce08 <rradtke1208@gmail.com>
2024-11-12 08:19:02 -0500

    More C++

---
+++ C++/prog2/Prog2/Prog2/Prog2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace::std;
+
+int main()
+{
+	// Ryan Radtke
+	cout << "A forward slash looks like / and a backslash looks like \\" << endl;
+	cout << 2000 << " is the first year of the century" << endl;
+	cout << "She said, \"I\'m tired of this.\"" << endl;
+	cout << "\t x \t y \n \t 1\t 2" << endl;
+	cout << "The character \'A\' has an ASCII value of 65" << endl;
+	cout << "Hi. " << "My " << "Name " << "Is " << "Ryan" << endl;
+	cin.get();
+	return 0;
+}
