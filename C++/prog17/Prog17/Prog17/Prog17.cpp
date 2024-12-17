59ee0ae6cd2b680bc328b6aff753da756b0c4298
Ryanforce08 <rradtke1208@gmail.com>
2024-12-17 08:22:56 -0500

    prog17

--- C++/prog17/Prog17/Prog17/Prog17.cpp
+++ C++/prog17/Prog17/Prog17/Prog17.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace::std;
+
+int main() {
+	srand(time(NULL));
+	int arr[1000];
+
+	for (int i = 0; i <= 1000; ++i) {
+		
+		arr[i] = rand() % (100 - 200 + 1) + 1;
+	}
+		
+
+	cin.get();
+	return 0;
+}
