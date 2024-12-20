32f413deb4a5ee0b6d4aa28b463fc96f2a45453f
Ryanforce08 <rradtke1208@gmail.com>
2024-12-20 08:23:45 -0500

    prog18

--- C++/prog18/Prog18/Prog18/Prog18.cpp
+++ C++/prog18/Prog18/Prog18/Prog18.cpp
@@ -22,24 +22,41 @@ void printIntArr(int a[], int length)
 
 	cout << s << endl;
 }
+void bubbleSort(int a[], int n)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				int swap = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = swap;
+			}
+		}
+	}
+}
+
 
 int main() {
 	srand(time(NULL));
 	int arr[1000];
-	
+
 	double a = 0;
 	int size = sizeof(arr) / sizeof(int);
 
 	for (int i = 0; i <= 999; ++i) {
-		
-		arr[i] = rand() % 101 + 100;
+
+		arr[i] = rand() % 1001 + 1000;
 	}
 	for (int i = 0; i < 999; ++i) {
 		a += arr[i];
 	}
-	
-	printIntArr(arr, sizeof(arr) / sizeof(int));
-	
+	bubbleSort(arr, size);
+	printIntArr(arr, size);
+
+	cout << "mean: " << a << "/" << size << "=" << a / size << endl;
 	cout << "mean: " << a << "/" << size << "=" << a / size << endl;
 	cin.get();
 	return 0;
