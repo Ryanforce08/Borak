ed4f540a9b1cdd8897cc77b9057993e2c800beb0
Ryanforce08 <rradtke1208@gmail.com>
2025-03-19 12:53:10 -0400

    ruby4

---
+++ Ruby/Ruby4/ruby4.rb
@@ -0,0 +1,14 @@
+counter = 0 
+while (counter < 10)
+    print counter
+    counter += 1
+end 
+for i in 0..5
+    print i
+end 
+counter = 0
+until (counter == 4)
+   print "\nhi"
+   counter += 1
+end
+4.times {print "\nhello"}
