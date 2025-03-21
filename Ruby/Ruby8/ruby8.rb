9bf8913a661e68ca18b5811042b3ad56edbaadb9
Ryanforce08 <rradtke1208@gmail.com>
2025-03-21 12:25:04 -0400

    ruby 8

--- Ruby/Ruby8/ruby8.rb
+++ Ruby/Ruby8/ruby8.rb
@@ -1,15 +1,10 @@
 
-def bubbleSort(a[])
-    swap
-    c = 0
-    while c < a.length - 1
-        d = 0
-        for d < a.length - c - 1
-            if (a[d] > a[d+1]) /* For descending order use < */
-            {
-                swap = a[d];
-                a[d] = a[d + 1];
-                a[d + 1] = swap;
-            }
-            d += 1
-        c +=1
+load 'bubblesort.rb'
+
+arr = []
+
+for i in 0..1000
+    arr[i] = rand(1001)
+end 
+bubble_sort(arr)
+puts arr
